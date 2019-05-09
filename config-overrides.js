a2bc5db7659bafb7633ffecd06c202cfd588db06
Trinh Chin <trinhchin@Trinhs-MacBook-Pro.local>
2019-05-09 16:40:00 +0700

    Add webpack-bundle-analyzer

--- config-overrides.js
+++ config-overrides.js
@@ -1,8 +1,10 @@
 const { override, fixBabelImports, addLessLoader } = require('customize-cra')
 const chalk = require('chalk')
 const ProgressBarPlugin = require('progress-bar-webpack-plugin')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+	.BundleAnalyzerPlugin
 
-const addProgressBarPlugin = () => config => {
+const addPlugins = () => config => {
 	config.plugins.push(
 		new ProgressBarPlugin({
 			format:
@@ -13,8 +15,18 @@ const addProgressBarPlugin = () => config => {
 				// chalk.green(':percent ') +
 				chalk.hex('#ffec3d')(':elapseds'),
 			// chalk.blue('( :elapsed s )')
-			complete: '◆',
-			incomplete: '◇'
+			complete: '▰',
+			incomplete: '▱'
+		}),
+		new BundleAnalyzerPlugin({
+			analyzerMode: 'disabled', // server, static, disabled
+			analyzerHost: 'localhost',
+			analyzerPort: 3001, // 8888
+			openAnalyzer: false, // true
+			// statsFilename: 'stats.json', // stats.json
+			// statsOptions: null, // null or {Object}
+			// generateStatsFile: true,
+			logLevel: 'info' // info, warn, error, silent
 		})
 	)
 	return config
@@ -30,5 +42,5 @@ module.exports = override(
 		javascriptEnabled: true,
 		modifyVars: { '@primary-color': '#ff4d4f' }
 	}),
-	addProgressBarPlugin()
+	addPlugins()
 )
