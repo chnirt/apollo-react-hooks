6bfcab84efdd94a1a8b27b3e15ab6c5b0ace785d
Trinh Chin <trinhchin@Trinhs-MacBook-Pro.local>
2019-05-30 23:54:26 +0700

    Fix wakeUp

--- src/App.js
+++ src/App.js
@@ -19,6 +19,13 @@ function App() {
 				.catch(err => {
 					console.log(err)
 				})
+			fetch('http://chnirt-apollo-server.herokuapp.com/graphql')
+				.then(res => {
+					console.log(res)
+				})
+				.catch(err => {
+					console.log(err)
+				})
 		}, 300000) // every 5 minutes (300000)
 		return () => clearInterval(wakeUp)
 	})
