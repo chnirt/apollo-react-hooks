591cfe2dc24995e266c5905a04d757f62f7ef192
chin trinh <trinhchinchin@gmail.com>
2019-07-11 15:34:04 +0700

    FIX

--- src/tools/apollo/index.js
+++ src/tools/apollo/index.js
@@ -4,7 +4,8 @@ import { ApolloLink, split } from 'apollo-link'
 import { HttpLink } from 'apollo-link-http'
 import { onError } from 'apollo-link-error'
 import { setContext } from 'apollo-link-context'
-import { WebSocketLink } from 'apollo-link-ws'
+// import { WebSocketLink } from 'apollo-link-ws'
+// import { getMainDefinition } from 'apollo-utilities'
 import store from '../mobx'
 
 const token = window.localStorage.getItem('access-token')
@@ -15,16 +16,29 @@ const httpLink = new HttpLink({ uri: 'http://localhost:4000/graphql' })
 // 	uri: 'https://chnirt-apollo-server.herokuapp.com/graphql'
 // })
 
-const wsLink = new WebSocketLink({
-	uri: `ws://localhost:4000/graphql`,
-	options: {
-		reconnect: true,
-		connectionParams: {
-			token: token ? token : '',
-			currentsite: currentsite ? currentsite : ''
-		}
-	}
-})
+// const wsLink = new WebSocketLink({
+// 	uri: `ws://localhost:4000/graphql`,
+// 	options: {
+// 		reconnect: true,
+// 		connectionParams: {
+// 			token: token ? token : '',
+// 			currentsite: currentsite ? currentsite : ''
+// 		}
+// 	}
+// })
+
+// const link = split(
+// 	// split based on operation type
+// 	({ query }) => {
+// 		const definition = getMainDefinition(query)
+// 		return (
+// 			definition.kind === 'OperationDefinition' &&
+// 			definition.operation === 'subscription'
+// 		)
+// 	},
+// 	wsLink,
+// 	httpLink
+// )
 
 const errorLink = new onError(({ graphQLErrors, networkError, operation }) => {
 	if (graphQLErrors) {
@@ -74,13 +88,11 @@ const defaultOptions = {
 	}
 }
 
-const requestLink = split(wsLink, httpLink)
-
 const client = new ApolloClient({
 	// cache: new InMemoryCache(),
-	defaultOptions,
+	// defaultOptions,
 	cache: new InMemoryCache().restore(window.__APOLLO_STATE__),
-	link: ApolloLink.from([authLink, errorLink, requestLink]),
+	link: ApolloLink.from([errorLink, authLink, httpLink]),
 	ssrForceFetchDelay: 100
 })
 
