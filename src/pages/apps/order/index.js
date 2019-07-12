5120d958f7efdeb7eca9e508d385a94f91542672
phong <phongnguyenabk@gmail.com>
2019-07-12 11:45:48 +0700

    pull

--- src/pages/apps/order/index.js
+++ src/pages/apps/order/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { Select, Button, List } from 'antd'
 import { withApollo } from 'react-apollo'
 import gql from 'graphql-tag'
-// import './index.css'
 
 class Order extends React.Component {
 	state = {
@@ -17,9 +16,12 @@ class Order extends React.Component {
 		this.handleDefaultDishes()
 	}
 
-	handleDefaultDishes () {
-		this.props.client.query({
-			query: MENU_BY_SELECTED_SITE
+	async handleDefaultDishes () {
+		await this.props.client.query({
+			query: MENU_BY_SELECTED_SITE,
+			variables: {
+				siteId: window.localStorage.getItem('currentsite')
+			}	
 		})
 		.then(res => {
 			if (res.data.menuPublishBySite.isPublished === true && res.data.menuPublishBySite.isActived === true) {
@@ -37,9 +39,11 @@ class Order extends React.Component {
 
 	async handleChange (selectedItems) {
 		await window.localStorage.setItem('currentsite', selectedItems)
-		await this.props.client.cache.reset()
 		await this.props.client.query({
-			query: MENU_BY_SELECTED_SITE
+			query: MENU_BY_SELECTED_SITE,
+			variables: {
+				siteId: window.localStorage.getItem('currentsite')
+			}	
 		})
 		.then(res => {
 			if (res.data.menuPublishBySite.isPublished === true && res.data.menuPublishBySite.isActived) {
@@ -54,139 +58,80 @@ class Order extends React.Component {
 			console.log(error)
 		})
 	}
+
+	async createOrder (item) {
+		await this.props.client.mutate({
+			mutation: ORDER_DISH,
+			variables: {
+				input: {
+					menuId: this.state.menuId,
+					dishId: item._id,
+					count: item.orderNumber
+				}
+			}
+		})
+		.then((res) => {
+			(res.data.orderDish)
+			? console.log('success')
+			: console.log('something went wrong')
+		})
+		.catch((error) => {
+			console.dir(error)
+		})
+	}
+
+	async selectDishHandler (index, item) {
+		let theDish = [...this.state.dishes]
+		await this.state.dishes.map(dish =>
+			(dish._id === item._id && dish.orderNumber < item.count)
+			? theDish[index] = {...theDish[index], orderNumber: item.orderNumber++}
+			: theDish[index] = {...theDish[index], orderNumber: item.orderNumber}
+		)
+
+		await this.setState({dishes: theDish})
+		await this.createOrder(item)
+	}
+
+	async unselectDishHandler (index, item) {
+		let theDish = [...this.state.dishes]
+		await this.state.dishes.map(dish =>
+			(dish._id === item._id && dish.orderNumber > 0)
+			? theDish[index] = {...theDish[index], orderNumber: item.orderNumber--}
+			: theDish[index] = {...theDish[index], orderNumber: item.orderNumber}
+		)
+
+		await this.setState({dishes: theDish})
+		await this.createOrder(item)
+	}
 	
-	handleActions(item) {
-		const x = this.state.dishes.map(dish => 'chay gium').indexOf(item._id)
-    if (this.handleActions === 'plus') {
-			// const x = this.state.dishes.map(dish => 'chay gium').indexOf(item._id)
-			this.selectDishHandler(x,item)
-    } else {
-			// const x = this.state.dishes.map(dish => 'chay ho').indexOf(item._id)
-			this.unselectDishHandler(x,item)
-    }
-  }
-
-	handleMinus (item) {
-		const x = this.state.dishes.map(dish => 'chay ho').indexOf(item._id)
-		this.unselectDishHandler(x,item)
-		// console.log(item._id)
-		// this.state.dishes.map(dish =>
-		// 	(item._id === dish._id) ? 
-		// 		// this.setState({
-		// 		// 	dishCountOrdered: this.state.dishCountOrdered-1
-		// 		// })
-		// 		: null
-		// )
-		// await this.props.client.mutate({
-		// 	mutation: ORDER_DISH,
-		// 	variables: {
-		// 		input: {
-		// 			menuId: this.state.menuId,
-		// 			dishId: item._id,
-		// 			count: 2
-		// 		}
-		// 	}
-		// })
-		// .then((res) => {
-		// 	console.log(res)
-		// 	let a = this.state.dishCountOrdered
-		// 	this.setState({
-		// 		dishCountOrdered: a--
-		// 	})
-		// 	console.log(this.state.dishCountOrdered)
-		// })
-		// .catch((error) => {
-		// 	console.dir(error)
-		// })
+	async handleMinus (item) {
+		const index = this.state.dishes.map(dish => '-').indexOf(item._id)
+		await this.unselectDishHandler(index,item)
 	}
 
-	selectDishHandler (id, item) {
-		let theDish = [...this.state.dishes]
-		theDish[id] = {...theDish[id], orderNumber: item.orderNumber++}
-		console.log(theDish[id].orderNumber)
+	async handlePlus (item) {
+		const index = this.state.dishes.map(dish => '+').indexOf(item._id)
+		this.selectDishHandler(index, item)
+	}
 
-		this.setState({dishes: theDish}, () => {
-			console.log(this.state.dishes[id].orderNumber)
+	handleConfirmOrder (e) {
+		this.props.client.mutate({
+			mutation: ORDER_DISH,
+			variables: {
+				input: {
+					menuId: this.state.menuId,
+					dishId: this.state.dishes[0]._id,
+					count: 1
+				}
+			}
+		})
+		.then((res) => {
+			console.log(res)
+		})
+		.catch((error) => {
+			console.dir(error)
 		})
 	}
-
-	unselectDishHandler (id, item) {
-		let theDish = [...this.state.dishes]
-		theDish[id] = {...theDish[id], orderNumber: item.orderNumber--}
-		console.log(theDish[id].orderNumber)
-
-		this.setState({dishes: theDish}, () => {
-			console.log(this.state.dishes[id].orderNumber)
-		})
-	}
-
-	handlePlus (item) {
-		const x = this.state.dishes.map(dish => 'chay gium').indexOf(item._id)
-		this.selectDishHandler(x,item)
-		
-		// await this.props.client.mutate({
-		// 	mutation: ORDER_DISH,
-		// 	variables: {
-		// 		input: {
-		// 			menuId: this.state.menuId,
-		// 			dishId: item._id,
-		// 			count: 2
-		// 		}
-		// 	}
-		// })
-		// .then((res) => {
-		// 	console.log(res)
-		// 	// this.setState({
-		// 	// 	dishCountOrdered: this.state.dishCountOrdered++
-		// 	// })
-		// 	this.setState({ dishCountOrdered: this.myRef.current.value+1})
-		// 	console.log(this.state.dishCountOrdered)
-		// })
-		// .catch((error) => {
-		// 	console.dir(error)
-		// })
-	}
-
-	handleCountOrder (number) {
-		// let count = number
-		// this.props.client.mutate({
-		// 	mutation: ORDER_DISH,
-		// 	variables: {
-		// 		input: {
-		// 			menuId: this.state.menuId,
-		// 			dishId: item._id,
-		// 			count: 1
-		// 		}
-		// 	}
-		// })
-		// .then(res => {
-		// 	console.log(res.data)
-		// 	console.log(count)
-		// })
-		// .catch((error) => {
-		// 	console.dir(error)
-		// })
-		return number
-	}
-
-	// handleConfirmOrder (e) {
-	// 	this.props.client.mutate({
-	// 		mutation: ORDER_DISH,
-	// 		variables: {
-	// 			input: {
-	// 				menuId: this.state.menuId,
-	// 				dishId: this.state.dishes[0]._id,
-	// 				count: 1
-	// 			}
-	// 		}
-	// 	})
-	// 	.then((res) => {
-	// 		console.log(res)
-	// 	})
-	// 	.catch((error) => {
-	// 		console.dir(error)
-	// 	})
-	// }
 
 	totalOrder (item) {
 		let total = 0
@@ -213,7 +158,6 @@ class Order extends React.Component {
 	}
 
 	render() {
-		console.log(this.state.dishes)
 		const currentsite = window.localStorage.getItem('currentsite')
 		const options = JSON.parse(window.localStorage.getItem('sites')).map(item =>
 			<Select.Option value={item._id} key={item._id}>
@@ -224,7 +168,7 @@ class Order extends React.Component {
 			<React.Fragment>
 				<Select
 					style={{ width: '100%', marginBottom: 20 }}
-					placeholder='Chọn Site'
+					placeholder='Chọn khu vực'
 					defaultValue={currentsite}
 					onChange={() => this.setState({dishes: []})}
 					onSelect={(e) => this.handleChange(e)}
@@ -247,7 +191,6 @@ class Order extends React.Component {
 										title={item.name}
 										description={`${this.totalOrder(item)}/${item.count}`}
 									/>
-									{/* <div ref={myRef}>{this.state.dishCountOrdered}</div> */}
 									<div>{item.orderNumber}</div>
 								</List.Item>
 							)}
@@ -268,15 +211,15 @@ class Order extends React.Component {
 							:	'Hệ thống đã khóa'	
 						)
 				}
-				<Button style={{ display: 'block', textAlign: 'center' }}>Order</Button>
+				<Button style={{ display: 'block', textAlign: 'center' }}>Xác nhận đã ăn</Button>
 			</React.Fragment>
 		)
 	}
 }
 
 const MENU_BY_SELECTED_SITE = gql`
-	{
-		menuPublishBySite {
+	query menuPublishBySite($siteId: String!) {
+		menuPublishBySite(siteId: $siteId) {
 			_id
 			name
 			siteId
@@ -294,21 +237,21 @@ const MENU_BY_SELECTED_SITE = gql`
 	}
 `
 
-const ORDERS_DISH = gql`
-	query ordersDish($menuId: String!, $dishId: String!) {
-		ordersDish(menuId: $menuId, dishId: $dishId) {
-			_id
-			userId
-			menuId
-			dishId
-			note
-			count
-			isConfirmed
-			createdAt
-			updatedAt
-		}  
-	}
-`
+// const ORDERS_DISH = gql`
+// 	query ordersDish($menuId: String!, $dishId: String!) {
+// 		ordersDish(menuId: $menuId, dishId: $dishId) {
+// 			_id
+// 			userId
+// 			menuId
+// 			dishId
+// 			note
+// 			count
+// 			isConfirmed
+// 			createdAt
+// 			updatedAt
+// 		}  
+// 	}
+// `
 
 const ORDER_DISH = gql`
 	mutation orderDish($input: CreateOrderInput!) {
@@ -316,10 +259,10 @@ const ORDER_DISH = gql`
 	}
 `
 
-const UPDATE_DISH = gql`
-	mutation updateDish($menuId: String!, $dishId: String!, $dishInput: DishInput!) {
-		updateDish(menuId: $menuId, dishId: $dishId, dishInput: $dishInput)
-	}
-`
+// const UPDATE_DISH = gql`
+// 	mutation updateDish($menuId: String!, $dishId: String!, $dishInput: DishInput!) {
+// 		updateDish(menuId: $menuId, dishId: $dishId, dishInput: $dishInput)
+// 	}
+// `
 
 export default withApollo(Order)
