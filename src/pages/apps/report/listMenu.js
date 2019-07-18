b1f3bce3836f4475a260aa99914b589994fe2e9f
minhtoan <duongminhtoan137@gmail.com>
2019-07-18 15:33:44 +0700

    fe report thur

---
+++ src/pages/apps/report/listMenu.js
@@ -0,0 +1,201 @@
+import React from 'react'
+import { Collapse, Button } from 'antd'
+import gql from 'graphql-tag'
+import XLSX from 'xlsx'
+import ListUser from './listUser'
+import { HOCQueryMutation } from '../../../components/shared/hocQueryAndMutation'
+
+import './index.css'
+
+const { Panel } = Collapse
+
+class listMenu extends React.Component {
+	state = {
+		isActive: false
+	}
+
+	changeActive = () => {
+		this.setState(prevState => ({ isActive: !prevState.isActive }))
+	}
+
+	export(menu) {
+		const dishes = []
+		const { getOrderByMenu } = this.props
+		// menu.dishes.forEach(item => dishes.push([item.name, '', '', item.count]))
+		menu.dishes.forEach(item => {
+			const a = [item.name, '', '']
+			return dishes.push(a)
+		})
+		getOrderByMenu.ordersByMenu.forEach((order, i) =>
+			dishes[i].push(order.count)
+		)
+		dishes.unshift(['Tên món ăn', '', '', 'Số lượng'])
+		// console.log(dishes)
+		const wb = XLSX.utils.book_new()
+		const ws = XLSX.utils.aoa_to_sheet(dishes)
+
+		XLSX.utils.book_append_sheet(wb, ws, 'Sheet1')
+		XLSX.writeFile(wb, `${menu.name}.xlsx`, { bookType: 'xlsx' })
+	}
+
+	render() {
+		const { menu, isLock, isActiveProps, getOrderByMenu, menuId } = this.props
+		const { isActive } = this.state
+
+		return (
+			<Collapse onChange={this.changeActive}>
+				<Panel
+					extra={
+						<>
+							<Button
+								className="publish style-btn"
+								onClick={e => isLock(e, menu._id)}
+							>
+								{menu.isLocked ? 'Unlock' : 'Lock'}
+							</Button>
+
+							<Button
+								className="publish style-btn"
+								onClick={e => isActiveProps(e, menu._id)}
+							>
+								Complete
+							</Button>
+
+							<Button
+								className="style-btn"
+								onClick={() => this.export(menu)}
+								variant="raised"
+								color="secondary"
+								disabled={!isActive}
+							>
+								Request
+							</Button>
+						</>
+					}
+					header={menu.name}
+					key="1"
+					disabled={!!menu.isLocked}
+				>
+					<Collapse>
+						{menu.dishes &&
+							menu.dishes.map(dish => {
+								// console.log(dish)
+								return (
+									<Panel header={`${dish.name} x${dish.count}`} key={dish._id}>
+										{getOrderByMenu.ordersByMenu &&
+											getOrderByMenu.ordersByMenu.map(orderByMenu => {
+												return (
+													<ListUser
+														dishCount={dish.count}
+														menuId={menuId}
+														orderByMenu={orderByMenu}
+														key={orderByMenu._id}
+														userId={orderByMenu.userId}
+														countProps={orderByMenu.count}
+														dishId={dish._id}
+													/>
+												)
+											})}
+									</Panel>
+								)
+							})}
+					</Collapse>
+				</Panel>
+			</Collapse>
+		)
+	}
+}
+
+const GET_MENU_BY_SITE = gql`
+	query menusBySite($siteId: String!) {
+		menusBySite(siteId: $siteId) {
+			_id
+			name
+			isActive
+			isLocked
+			dishes {
+				name
+				count
+				_id
+			}
+		}
+	}
+`
+
+const LOCK_AND_UNLOCK_MENU = gql`
+	mutation lockAndUnlockMenu($id: String!) {
+		lockAndUnlockMenu(id: $id)
+	}
+`
+
+const CLOSE_MENU = gql`
+	mutation closeMenu($id: String!) {
+		closeMenu(id: $id)
+	}
+`
+
+const ORDER_BY_MENU = gql`
+	query ordersByMenu($menuId: String!) {
+		ordersByMenu(menuId: $menuId) {
+			userId
+			dishId
+			count
+			_id
+		}
+	}
+`
+
+const GET_USER_NAME = gql`
+	query user($_id: String!) {
+		user(_id: $_id) {
+			username
+			fullName
+		}
+	}
+`
+
+export default HOCQueryMutation([
+	{
+		query: GET_MENU_BY_SITE,
+		name: 'getMenuBySite',
+		options: () => {
+			return {
+				variables: {
+					siteId: localStorage.getItem('currentsite')
+				}
+			}
+		}
+	},
+	{
+		query: ORDER_BY_MENU,
+		name: 'getOrderByMenu',
+		options: props => {
+			return {
+				variables: {
+					menuId: props.menuId
+				}
+			}
+		}
+	},
+	{
+		query: GET_USER_NAME,
+		name: 'getUserName',
+		options: () => {
+			return {
+				variables: {
+					_id: '40eb5c20-9e41-11e9-8ded-f5462f3a1447'
+				}
+			}
+		}
+	},
+	{
+		mutation: LOCK_AND_UNLOCK_MENU,
+		name: 'lockAndUnLockMenu',
+		option: {}
+	},
+	{
+		mutation: CLOSE_MENU,
+		name: 'closeMenu',
+		option: {}
+	}
+])(listMenu)
---
+++ src/pages/apps/report/listUser.js
@@ -0,0 +1,169 @@
+import React from 'react'
+import { Icon, Button } from 'antd'
+import gql from 'graphql-tag'
+import { HOCQueryMutation } from '../../../components/shared/hocQueryAndMutation'
+import './index.css'
+
+class listUser extends React.Component {
+	handlePlus = () => {
+		const { mutate, menuId, dishId, dishCount, countProps } = this.props
+
+		if (countProps < dishCount) {
+			mutate
+				.orderDish({
+					mutation: ORDER_DISH,
+					variables: {
+						input: {
+							menuId,
+							dishId,
+							count: countProps + 1
+						}
+					},
+					refetchQueries: () => [
+						{
+							query: ORDER_BY_MENU,
+							variables: {
+								menuId
+							}
+						}
+					]
+				})
+				.then(() => {
+					// console.log(res)
+				})
+				.catch(err => {
+					console.log(err)
+				})
+		}
+	}
+
+	handleMinus = () => {
+		const { mutate, menuId, dishId, countProps } = this.props
+
+		if (countProps > 0) {
+			mutate
+				.orderDish({
+					mutation: ORDER_DISH,
+					variables: {
+						input: {
+							menuId,
+							dishId,
+							count: countProps - 1
+						}
+					},
+					refetchQueries: () => [
+						{
+							query: ORDER_BY_MENU,
+							variables: {
+								menuId
+							}
+						}
+					]
+				})
+				.then(() => {
+					// console.log(res)
+				})
+				.catch(err => {
+					console.log(err)
+				})
+		}
+	}
+
+	render() {
+		const {
+			orderByMenu,
+			dishId,
+			getUserName,
+			dishCount,
+			countProps
+		} = this.props
+		console.log(this)
+		return (
+			<>
+				{orderByMenu.dishId === dishId ? (
+					<div
+						style={{
+							display: 'flex',
+							justifyContent: 'space-between',
+							alignItems: 'center'
+						}}
+					>
+						{`${getUserName.user.fullName} ${countProps}/${dishCount}`}
+						<div>
+							<Button
+								disabled={countProps === 0}
+								style={{ marginRight: 10 }}
+								onClick={() => this.handleMinus(countProps)}
+							>
+								<Icon type="minus" />
+							</Button>
+							<Button
+								disabled={countProps === dishCount}
+								onClick={() => this.handlePlus(countProps)}
+							>
+								<Icon type="plus" />
+							</Button>
+						</div>
+					</div>
+				) : null}
+			</>
+		)
+	}
+}
+
+const GET_USER_NAME = gql`
+	query user($_id: String!) {
+		user(_id: $_id) {
+			username
+			fullName
+		}
+	}
+`
+
+const ORDER_DISH = gql`
+	mutation orderDish($input: CreateOrderInput!) {
+		orderDish(input: $input)
+	}
+`
+
+const ORDER_BY_MENU = gql`
+	query ordersByMenu($menuId: String!) {
+		ordersByMenu(menuId: $menuId) {
+			userId
+			dishId
+			count
+			_id
+		}
+	}
+`
+
+export default HOCQueryMutation([
+	{
+		query: GET_USER_NAME,
+		name: 'getUserName',
+		options: props => {
+			return {
+				variables: {
+					_id: props.userId
+				}
+			}
+		}
+	},
+	{
+		query: ORDER_BY_MENU,
+		name: 'getOrderByMenu',
+		options: props => {
+			console.log(props)
+			return {
+				variables: {
+					menuId: props.menuId
+				}
+			}
+		}
+	},
+	{
+		mutation: ORDER_DISH,
+		name: 'orderDish',
+		option: {}
+	}
+])(listUser)
